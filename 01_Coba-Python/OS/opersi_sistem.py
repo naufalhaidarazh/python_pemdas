8b056717a3dea36b50de6fa1c2d973d2451c86e9
naufalhaidarazh <naufaldaraz19@gmail.com>
2024-10-01 10:19:45 +0700

    pertemuan 2 - update

--- 01_Coba-Python/OS/opersi_sistem.py
+++ 01_Coba-Python/OS/opersi_sistem.py
@@ -1,7 +1,7 @@
 angka1=10
 angka2=8
 
-print("Hasil dari angka1 + angka2 adalah ", angka1+angka2)
-print("Hasil dari angka1 - angka2 adalah ", angka1-angka2)
-print("Hasil dari angka1 * angka2 adalah ", angka1*angka2)
-print("Hasil dari angka1 / angka2 adalah ", angka1/angka2)
+print("Hasil dari", angka1, "+", angka2, "adalah ", angka1+angka2)
+print("Hasil dari", angka1, "-", angka2, "adalah ", angka1-angka2)
+print("Hasil dari", angka1, "*", angka2, "adalah ", angka1*angka2)
+print("Hasil dari", angka1, "/", angka2, "adalah ", angka1/angka2)
