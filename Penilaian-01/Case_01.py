2c353131676f49c9d1604216af056ad7e47e4a03
naufalhaidarazh <naufaldaraz19@gmail.com>
2024-10-08 08:37:33 +0700

    Penilaian 1 : Case 1

---
+++ Penilaian-01/Case_01.py
@@ -0,0 +1,16 @@
+# (Soal) Seorang pengguna memiliki sejumlah uang. Dia membeli beberapa barang dengan harga yang sudah ditentukan. Buatlah program Pythonn yang menghitung sisa uang setelah belanja. Program meminta input total uang yang dimiliki dan total biaya belanja. Tampilkan sisa uang setelah pembelian
+
+#header
+print(52*"=")
+print("Studi Kasus 1 : Mneghitung Sisa Uang Setelah Belanja")
+print(52*"=")
+
+#variabel
+Uang = int(input("Masukkan uang pertama anda (Rp.) = "))
+Barang = int(input("Masukkan harga barang yang anda beli (Rp.) = "))
+
+Sisa = Uang - Barang
+
+#output
+print(f"Sisa uang anda setelah membeli barang tersebut adalah Rp. {Sisa}", "\n")
+print("Operasi Berhasil, Terimakasih Karena Sudah Mau Mencoba >.<")
